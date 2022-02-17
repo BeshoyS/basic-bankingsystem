7e55a2bdcc0bb49bb5a348e92e3a4479950b9dcc
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2022-02-17 18:33:13 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './Components/App/App';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router basename='/basic-banking-app/'>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 
